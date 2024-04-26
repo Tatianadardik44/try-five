2e1965030743b0e5079139e0976089859c0abf9d
Tatianadardik44 <149703862+Tatianadardik44@users.noreply.github.com>
2024-04-26 13:32:14 +0000

    Deploying to gh-pages from @ Tatianadardik44/try-five@0a80cfcde8e839be8e30bca8bfc82ce434ec6f48 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-d61c921b.js";import{A as o}from"./assets/vendor-8f98089f.js";const r=Array.from(document.querySelectorAll(".accordion-container"));new o(r,{duration:400,showMultiple:!0,activeClass:"is-active"});
+import"./assets/styles-b3276fec.js";import{A as o}from"./assets/vendor-8f98089f.js";const r=Array.from(document.querySelectorAll(".accordion-container"));new o(r,{duration:400,showMultiple:!0,activeClass:"is-active"});
 //# sourceMappingURL=commonHelpers2.js.map

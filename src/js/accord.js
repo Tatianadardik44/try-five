0810a3359af5c4feed89b72e26ee41bab1247357
Tatianadardik44 <dardiktatiana@gmail.com>
2024-04-26 08:44:12 +0200

    desctop

--- src/js/accord.js
+++ src/js/accord.js
@@ -1,4 +1,5 @@
 import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 const accordions = Array.from(document.querySelectorAll('.accordion-container'));
 new Accordion(accordions, {duration: 400,
   showMultiple: true});

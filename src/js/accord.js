1e604490716e9c1d77802b0f5de74e78ace167f0
Tatianadardik44 <dardiktatiana@gmail.com>
2024-04-27 12:48:19 +0200

    style-decktop

--- src/js/accord.js
+++ src/js/accord.js
@@ -2,5 +2,5 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 const accordions = Array.from(document.querySelectorAll('.accordion-container'));
 new Accordion(accordions, {duration: 400,
-    showMultiple: true,
-  activeClass: 'is-active'});
+    showMultiple: true
+  });
